8dacc52edf5c71e350525f03a400167ca497fb9d
Magnetica1 <jackkw818@gmail.com>
2023-02-10 21:44:44 -0500

    Finish placement and scroll

--- three-portfolio/main.js
+++ three-portfolio/main.js
@@ -4,8 +4,11 @@ import *as THREE from "three"
 
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Camera, CubeCamera, Group, Loader } from "three";
+
 
 //Setup main elements
 const scene = new THREE.Scene();
@@ -19,10 +22,9 @@ const renderer = new THREE.WebGLRenderer({
 //Set the canvas(renderer) to the window height and width
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x0c0924)
 //change camera position
 camera.position.set(0, 15, 25);
-camera.rotateZ(11)
-
 
 const positioner = new THREE.AxesHelper
 scene.add(positioner)
@@ -46,6 +48,9 @@ scene.add(pointLight, floodLight)
 pointLight.position.set(0, 10, 20)
 torus.translateY(0);
 
+
+//OBJECT CREATOR
+
 const group = new THREE.Mesh();
 const rotateAround = new THREE.Mesh();
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -58,6 +63,9 @@ group.rotation.set(0, 4.7, 0)
 group.scale.set(10, 10 ,10)
 group.position.set(0, 0, 0)
 rotateAround.scale.set(10, 10 ,10)  
+
+
+// 3D OBJECT LOADER
 
 loader.load( './htmlLogo.glb', function ( gltf ) {
   const html = gltf.scene
@@ -82,18 +90,56 @@ loader.load( './github.glb', function ( gltf ) {
 
 loader.load( './unity.glb', function ( gltf ) {
   const unity = gltf.scene
-  unity.rotateY(86.4);
-  unity.position.set(0, 0, 15);
+  unity.rotation.set(-39, -1.5, 0)
+  unity.position.set(0, 0, 35);
   scene.add(unity)
 });
+loader.load( './unityText.glb', function ( gltf ) {
+  const uText = gltf.scene
+  uText.rotateY(0);
+  uText.position.set(0, 0, 0);
+  scene.add(uText)
+});
+
+// Text Randomizer
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 20);
+}
  
+
+// Camera Movement
+
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
 
-  camera.position.z = t * -0.03;
-  if(t <= -1000) {
-    camera.rotation.y = t * 0.002;
-  }
+    // camera.position.z = t * -0.03;
+    camera.rotation.x = t * 0.0044;
   console.log(t)
 }
 
@@ -101,7 +147,7 @@ document.body.onscroll = moveCamera;
 moveCamera();
 
 
-moveCamera();
+//Animation
 
 //Is like a gameloop and updates the website
 function animate() {
